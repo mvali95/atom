b9c14984d90adfb0de7791c9555d04be5036dd59
Nathan Sobo <nathan@github.com>
2017-10-24 17:27:14 -0600

    Include test fixtures in ASAR bundle to support legacy package tests

--- script/lib/copy-assets.js
+++ script/lib/copy-assets.js
@@ -19,17 +19,24 @@ module.exports = function () {
     path.join(CONFIG.repositoryRootPath, 'package.json'),
     path.join(CONFIG.repositoryRootPath, 'static'),
     path.join(CONFIG.repositoryRootPath, 'src'),
-    path.join(CONFIG.repositoryRootPath, 'vendor')
+    path.join(CONFIG.repositoryRootPath, 'vendor'),
+    // The legacy test environment promises package tests that fixtures are available, so we need to ship them
+    path.join(CONFIG.repositoryRootPath, 'spec', 'fixtures')
   ]
   srcPaths = srcPaths.concat(glob.sync(path.join(CONFIG.repositoryRootPath, 'spec', '*.*'), {ignore: path.join('**', '*-spec.*')}))
   for (let srcPath of srcPaths) {
     fs.copySync(srcPath, computeDestinationPath(srcPath), {filter: includePathInPackagedApp})
   }
-
   fs.copySync(
     path.join(CONFIG.repositoryRootPath, 'resources', 'app-icons', CONFIG.channel, 'png', '1024.png'),
     path.join(CONFIG.intermediateAppPath, 'resources', 'atom.png')
   )
+
+  // If evil-files are included, the asar bundle gets corrupted, but a
+  // newline in one the file names breaks the ability to ignore them with a
+  // glob pattern. Let's just delete them after the copy instead.
+  const evilFilesPath = path.join(CONFIG.intermediateAppPath, 'spec', 'fixtures', 'evil-files')
+  if (fs.existsSync(evilFilesPath)) fs.removeSync(evilFilesPath)
 }
 
 function computeDestinationPath (srcPath) {
